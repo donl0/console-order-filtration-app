5c61c8ebbd23a6cf9aa780e6c1d1bd172dc2d06e
donl0 <donl_117n@mail.ru>
2024-10-23 21:07:46 +0300

    Implement Distric Repository

---
+++ Infrastructure/Repositories/DistrictRepository.cs
@@ -0,0 +1,34 @@
+﻿using Domain.Models;
+using Infrastructure.Db.Interface;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories
+{
+    public class DistrictRepository : IDistrictRepository
+    {
+        private readonly IDbContext _dbContext;
+        private readonly CancellationToken _token;
+
+        public DistrictRepository(IDbContext dbContext, CancellationToken token)
+        {
+            _dbContext = dbContext;
+            _token = token;
+        }
+
+        public async Task<bool> CheckIfExistAsync(string name)
+        {
+            return await _dbContext.Districts.AnyAsync(d => d.Name == name);
+        }
+
+        public async Task CreateAsync(District district)
+        {
+            await _dbContext.Districts.AddAsync(district);
+            await _dbContext.SaveChangesAsync(_token);
+        }
+
+        public async Task<District> GetByNameAsync(string name)
+        {
+            return await _dbContext.Districts.FirstOrDefaultAsync(d => d.Name == name);
+        }
+    }
+}
