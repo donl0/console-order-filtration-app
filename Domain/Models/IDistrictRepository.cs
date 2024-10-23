1df80c9334b69dd7c6c89191daa502c51af60576
donl0 <donl_117n@mail.ru>
2024-10-23 18:37:35 +0300

    Add GetByName and CheckIfExist in district repository

--- Domain/Models/IDistrictRepository.cs
+++ Domain/Models/IDistrictRepository.cs
@@ -3,5 +3,7 @@
     public interface IDistrictRepository
     {
         public void Add(District district);
+        public District GetByNameAsync(string name);
+        public bool CheckIfExist(string name);
     }
 }
