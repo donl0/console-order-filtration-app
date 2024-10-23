2c30d10dc0e40f73e14cc0cdda7a3904e01f3e30
donl0 <donl_117n@mail.ru>
2024-10-23 18:10:53 +0300

    Add order model

---
+++ Domain/Models/Order.cs
@@ -0,0 +1,45 @@
+﻿using Domain.Exceptions;
+
+namespace Domain.Models
+{
+    public class Order : Entity
+    {
+        public Guid UniqueNumber { get; private set; }
+        public int Weight { get; private set; }
+        public DateTime DeliveryTime{ get; private set; }
+        public District District { get; private set; }
+
+        public Order() { }
+
+        public Order(Guid uniqueNumber, int weight, DateTime deliveryTime, string districtName)
+        {
+            UniqueNumber = uniqueNumber;
+            Weight = weight;
+            DeliveryTime = deliveryTime;
+            District = new District(districtName);
+
+            Validate();
+        }
+
+        private void Validate()
+        {
+            if (UniqueNumber == Guid.Empty)
+                throw new FieldIsNullOrEmptyException(nameof(UniqueNumber));
+
+            if (Weight == null)
+                throw new FieldIsNullOrEmptyException(nameof(Weight));
+
+            if (DeliveryTime == null)
+                throw new FieldIsNullOrEmptyException(nameof(DeliveryTime));
+
+            if (District == null)
+                throw new FieldIsNullOrEmptyException(nameof(District));
+
+            if (Weight <= 0)
+                throw new WeightExceptino(nameof(Weight));
+
+            if (DeliveryTime < DateTime.UtcNow)
+                throw new FutureTimeException(DeliveryTime.ToString());
+        }
+    }
+}
