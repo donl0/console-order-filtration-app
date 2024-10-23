7d7c62e3cf80e9d0b0536167c4fc5ff1f938e1f0
donl0 <donl_117n@mail.ru>
2024-10-23 19:15:56 +0300

    Add create order DTO

---
+++ Application/DTO/CreateOrderDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Application.DTO
+{
+    public class CreateOrderDTO
+    {
+        public Guid UniqueNumber;
+        public int Weight;
+        public DateTime DeliveryTime;
+        public string DistrictName;
+    }
+}
