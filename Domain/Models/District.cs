42d6d751765f7901878fc0aaefb025d8a54e8f43
donl0 <donl_117n@mail.ru>
2024-10-23 18:10:18 +0300

    Add district model

---
+++ Domain/Models/District.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Exceptions;
+
+namespace Domain.Models
+{
+    public class District : Entity
+    {
+        public string Name { get; private set; }
+
+        public District() { }
+
+        public District(string name)
+        {
+            Name = name;
+
+            Validate();
+        }
+
+        private void Validate()
+        {
+            if (Name == null)
+                throw new FieldIsNullOrEmptyException(nameof(Name));
+        }
+    }
+}
