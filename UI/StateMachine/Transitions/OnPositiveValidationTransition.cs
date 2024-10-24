9d4f59563bef3639ae59190917149fde29dbbeb3
donl0 <donl_117n@mail.ru>
2024-10-24 18:33:07 +0300

    Add transitions of validation result

---
+++ UI/StateMachine/Transitions/OnPositiveValidationTransition.cs
@@ -0,0 +1,23 @@
+﻿using UI.StateMachine.States.ValidateStates;
+using UI.StateMachine.StateSwitcher;
+
+namespace UI.StateMachine.Transitions
+{
+    internal class OnPositiveValidationTransition: BaseTransition<IValidatableState>
+    {
+        public OnPositiveValidationTransition(IStateSwitcher stateSwitcher, IValidatableState previousState, IValidatableState nextState) : base(stateSwitcher, previousState, nextState)
+        {
+        }
+
+        public override bool TryTransit()
+        {
+            if (CurrentState.IsValid == true)
+            {
+                GoNextState();
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
