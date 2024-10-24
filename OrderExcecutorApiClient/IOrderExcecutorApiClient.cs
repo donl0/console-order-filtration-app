e538aeff5936fbe2ea8d7c1e0dc7709b96a2b356
donl0 <donl_117n@mail.ru>
2024-10-25 00:27:48 +0300

    Add interface to api client

---
+++ OrderExcecutorApiClient/IOrderExcecutorApiClient.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Models.FilteredOrders;
+using Domain.Models.Orders;
+
+namespace ApiClient
+{
+    public interface IOrderExcecutorApiClient
+    {
+        Task<long> CreateOrderAsync(Guid uniqueNumber, int weight, DateTime deliveryTime, string districtName, CancellationToken cancellationToken);
+        Task<List<FilteredResult>> GetAllFilteredResultsAsync(CancellationToken cancellationToken);
+        Task<List<Order>> InitializeFilteringAsync(DateTime timeAfterFirstOrder, string districtName, CancellationToken cancellationToken);
+    }
+}
