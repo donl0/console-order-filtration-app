38a8a2eddf78078b43b181e8d0fa7e0f6b5f7fc6
donl0 <donl_117n@mail.ru>
2024-10-24 18:34:08 +0300

    Add mapper for transitions

---
+++ UI/StateMachine/TransitionMapGenerator/ReflectionTransitionMapGenerator.cs
@@ -0,0 +1,40 @@
+﻿using System.Reflection;
+using UI.StateMachine.States;
+using UI.StateMachine.StateSwitcher;
+using UI.StateMachine.Transitions;
+
+namespace UI.StateMachine.TransitionMapGenerator
+{
+    internal class ReflectionTransitionMapGenerator : ITransitionMapGenerator
+    {
+        public Dictionary<ITransitionableBaseState, List<IBaseTransition>> Generate(
+            List<ITransitionableBaseState> states,
+            List<IBaseTransition> transitionsContainer, IStateSwitcher stateSwitcher)
+        {
+            var transitionsMap = new Dictionary<ITransitionableBaseState, List<IBaseTransition>>();
+
+            foreach (var transition in transitionsContainer)
+            {
+                var transitionType = transition.GetType();
+
+                var currentStateField = transitionType.GetField("CurrentState", BindingFlags.Instance | BindingFlags.NonPublic);
+                var nextStateField = transitionType.GetField("NextState", BindingFlags.Instance | BindingFlags.NonPublic);
+
+                var currentState = currentStateField?.GetValue(transition) as ITransitionableBaseState;
+                var nextState = nextStateField?.GetValue(transition) as ITransitionableBaseState;
+
+                if (currentState != null && nextState != null)
+                {
+                    if (!transitionsMap.ContainsKey(currentState))
+                    {
+                        transitionsMap[currentState] = new List<IBaseTransition>();
+                    }
+
+                    transitionsMap[currentState].Add(transition);
+                }
+            }
+
+            return transitionsMap;
+        }
+    }
+}
