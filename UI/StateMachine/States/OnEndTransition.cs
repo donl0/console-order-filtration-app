6a4738a091a759827b20c940eea523a72593a13f
donl0 <donl_117n@mail.ru>
2024-10-25 00:24:49 +0300

    Add transition on end

---
+++ UI/StateMachine/States/OnEndTransition.cs
@@ -0,0 +1,24 @@
+﻿using UI.StateMachine.Transitions;
+
+namespace UI.StateMachine.States
+{
+    internal class OnEndTransition : BaseTransition
+    {
+        public readonly BaseState _nextState;
+
+        public OnEndTransition(BaseState nextState)
+        {
+            _nextState = nextState;
+        }
+
+        public override BaseState GetStateToSwitch()
+        {
+            return _nextState;
+        }
+
+        protected override bool CheckIsNeedInternalTransition()
+        {
+            return true;
+        }
+    }
+}
