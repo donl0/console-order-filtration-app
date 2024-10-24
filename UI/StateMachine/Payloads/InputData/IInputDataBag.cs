8fbd756f2cb165b857653ae6772f027cb8b95c7d
donl0 <donl_117n@mail.ru>
2024-10-24 18:30:08 +0300

    Add bag to save data during transitions

---
+++ UI/StateMachine/Payloads/InputData/IInputDataBag.cs
@@ -0,0 +1,11 @@
+﻿namespace UI.StateMachine.Payloads.InputData
+{
+    internal interface IInputDataBag
+    {
+        string LastInputData { get; }
+
+        T GetPayload<T>() where T : BasePayload;
+        void SetLastInput(string value);
+        void SetPayload(BasePayload value);
+    }
+}
---
+++ UI/StateMachine/Payloads/InputData/InputDataBag.cs
@@ -0,0 +1,41 @@
+﻿namespace UI.StateMachine.Payloads.InputData
+{
+    internal class InputDataBag : IInputDataBag
+    {
+        private string _lastInputData;
+
+        private List<BasePayload> _payloads = new List<BasePayload>();
+
+        public string LastInputData => _lastInputData;
+
+        public void SetLastInput(string value)
+        {
+            _lastInputData = value;
+        }
+
+        public void SetPayload(BasePayload value)
+        {
+            var findValue = _payloads.Find(p => p.GetType() == value.GetType());
+
+            if (findValue != null)
+            {
+                _payloads.Remove(findValue);
+
+            }
+
+            _payloads.Add(value);
+        }
+
+        public T GetPayload<T>() where T : BasePayload
+        {
+            foreach (var payload in _payloads)
+            {
+                if (payload is T typedPayload)
+                {
+                    return typedPayload;
+                }
+            }
+            throw new InvalidOperationException($"Payload of type {typeof(T)} not found.");
+        }
+    }
+}
