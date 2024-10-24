3396917dad95e59d667a79a263f8292cd1ba5d1e
donl0 <donl_117n@mail.ru>
2024-10-24 18:33:48 +0300

    Add state to get unique number

---
+++ UI/StateMachine/States/InputOrderNumberState.cs
@@ -0,0 +1,31 @@
+﻿using UI.StateMachine.Payloads;
+using UI.StateMachine.Payloads.InputData;
+using UI.StateMachine.States.ValidateStates;
+
+namespace UI.StateMachine.States
+{
+    internal class InputOrderNumberState : ValidateState
+    {
+        public InputOrderNumberState(IInputDataBag bag) : base(bag)
+        {
+        }
+
+        public override void ExecuteInput()
+        {
+            Console.WriteLine("Please enter a GUID:");
+
+            string input = Console.ReadLine();
+
+            if (Guid.TryParse(input, out Guid guid))
+            {
+                Bag.SetPayload(new UniqueNumberPayload(guid));
+                SetIsValid();
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid GUID.");
+                SetInvalid();
+            }
+        }
+    }
+}
