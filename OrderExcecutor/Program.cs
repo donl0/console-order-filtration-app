7995712f0b76f1e1eb278a65e6add86220a19559
donl0 <donl_117n@mail.ru>
2024-10-25 13:07:27 +0300

    Add serilog

--- OrderExcecutor/Program.cs
+++ OrderExcecutor/Program.cs
@@ -1,8 +1,22 @@
 using Application;
 using Infrastructure;
 using OrderExcecutor.Middleware;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var configuration = new ConfigurationBuilder()
+           .AddJsonFile("appsettings.json")
+           .Build();
+
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(configuration)
+    .WriteTo.Console()
+    .WriteTo.PostgreSQL(
+        configuration["DbConnection"],
+        "Logs",
+        needAutoCreateTable: true)
+    .CreateLogger();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
@@ -30,7 +44,7 @@ app.MapControllers();
 app.UseCors(x =>
 {
     x.WithHeaders().AllowAnyHeader();
-    x.WithOrigins("http://localhost:3000");
+    x.WithOrigins("http://localhost:6364");
     x.WithMethods().AllowAnyMethod();
 });
 
